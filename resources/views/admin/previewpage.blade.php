69d6b0cf5db8f974cd43798768d9ac7af2500f71
Abdul Hannan Danish <mrahdanish@gmail.com>
2023-12-15 21:52:17 +0500

    Enable the Function of Copying content on Preview Page

--- resources/views/admin/previewpage.blade.php
+++ resources/views/admin/previewpage.blade.php
@@ -2,6 +2,7 @@
 
 @push('stylesheet-page-level')
 @endpush
+
 @section('content')
     <div class="row">
         <div class="card">
@@ -28,16 +29,41 @@
 
                 </div>
                 <div class="card-footer">
-                    <button class="btn btn-success" onclick="javascript:alert('Content has been copied successfully.')">Copy
-                        Content</button>
+                    <button class="btn btn-success" id="copyContentBtn">Copy Content</button>
                 </div>
-
-
-
             </div>
         </div>
-        <!-- Row end -->
-    @endsection
+    </div>
 
     @push('script-page-level')
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                // Get the button element
+                var copyContentBtn = document.getElementById('copyContentBtn');
+
+                // Add click event listener to the button
+                copyContentBtn.addEventListener('click', function() {
+                    // Get the container element that holds the received data
+                    var receivedDataContainer = document.getElementById('receivedDataContainer');
+
+                    // Create a range object to select the content
+                    var range = document.createRange();
+                    range.selectNode(receivedDataContainer);
+
+                    // Select the content
+                    window.getSelection().removeAllRanges();
+                    window.getSelection().addRange(range);
+
+                    // Copy the selected content to the clipboard
+                    document.execCommand('copy');
+
+                    // Clear the selection
+                    window.getSelection().removeAllRanges();
+
+                    // Alert the user
+                    alert('Content has been copied successfully.');
+                });
+            });
+        </script>
     @endpush
+@endsection
