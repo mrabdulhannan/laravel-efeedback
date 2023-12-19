7a344f7af495597c09bc4bce3d399d4469e3c3ff
Abdul Hannan Danish <mrahdanish@gmail.com>
2023-12-19 19:59:54 +0500

    Fix the issue in higlighting Selected Text on Assessment Page

--- resources/views/admin/newassesment.blade.php
+++ resources/views/admin/newassesment.blade.php
@@ -4,8 +4,8 @@
 @endpush
 @section('content')
     <!-- Row start -->
-    <div class="row ">
-        <div class="col-xxl-12 ">
+    <div class="row">
+        <div class="col-xxl-12">
             <div class="card">
                 <div class="card-header">
                     <h3 class="card-title">New Assessment</h3>
@@ -19,12 +19,10 @@
                             </ul>
                         </div>
 
-
                         <!-- Main Content -->
                         <div class="col-9">
                             <div id="selectedItemData" contenteditable="true">
                                 <!-- Selected item's data will be dynamically added here -->
-
                             </div>
                             <!-- Button to Send Selected Data -->
                         </div>
@@ -37,147 +35,31 @@
         </div>
     </div>
     <!-- Row end -->
-@endsection
-
-@push('script-page-level')
-    {{-- <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            const itemList = document.getElementById('itemList');
-            const selectedItemData = document.getElementById('selectedItemData');
-            const selectedItems = new Set();
-
-            const items = <?= Auth::user()->definecategories ?>
-            // Populate sidebar with item IDs
-            items.forEach(item => {
-                const listItem = document.createElement('li');
-                listItem.className = 'list-group-item';
-                listItem.textContent = `${item.title}`;
-                listItem.addEventListener('click', () => toggleSelectedItem(item));
-                itemList.appendChild(listItem);
-            });
-
-            function toggleSelectedItem(item) {
-                // Toggle the selected state
-                if (selectedItems.has(item.id)) {
-                    selectedItems.delete(item.id);
-                } else {
-                    selectedItems.add(item.id);
-                }
-
-                // Render selected items' data in the main content area
-                // updateSidebarStyling();
-                renderSelectedItemsData();
-
-
-            }
-
-            function updateSidebarStyling() {
-                // Remove the 'active' class from all list items
-                const listItems = document.querySelectorAll('.list-group-item');
-                listItems.forEach(listItem => {
-                    listItem.classList.remove('active');
-                });
-
-                // Add the 'active' class to selected items
-                selectedItems.forEach(itemId => {
-                    const listItem = document.querySelector(`.list-group-item[data-item-id="${itemId}"]`);
-                    if (listItem) {
-                        listItem.classList.add('active');
-                    }
-                });
-            }
-
-            function renderSelectedItemsData() {
-                // Clear previous content
-                selectedItemData.innerHTML = '';
-
-                // Append data for selected items to the main content area
-                selectedItems.forEach(itemId => {
-                    const selectedItem = items.find(item => item.id === itemId);
-                    if (selectedItem) {
-                        selectedItemData.innerHTML += `
-                                                                                                    <div class="container">
-                                                                                                    <div class="row ">
-                                                                                                        <div class="col-md-12">
-                                                                                                            <div class="mb-3">
-                                                                                                                <input type="text" class="form-control mb-2" id="title" name="title" value="${selectedItem.title}">
-                                                                                                                <textarea class="form-control" id="description" name="description" rows="6">${selectedItem.description}</textarea>
-                        <hr />                                                                                    </div>
-                                                                                                        </div>
-                                                                                                    </div>
-                                                                                                </div>
-                                                                                                `;
-                    }
-                });
-            }
-
-            // Button click event handler
-            document.getElementById('sendDataButton').addEventListener('click', function() {
-                // Get data of selected items
-                const selectedData = getSelectedData();
-
-                // Redirect to another page with selected data
-                window.location.href = '/previewpage?selectedData=' + encodeURIComponent(JSON.stringify(selectedData));
-                // window.location.href = '{{route('previewpage')}}';
-            });
-
-            function getSelectedData() {
-                const selectedData = [];
-
-                // Collect data for selected items
-                selectedItems.forEach(itemId => {
-                    const selectedItem = items.find(item => item.id === itemId);
-                    if (selectedItem) {
-                        selectedData.push({
-                            id: selectedItem.id,
-                            title: selectedItem.title,
-                            description: selectedItem.description,
-                            type: selectedItem.type,
-                        });
-                    }
-                });
-
-                return selectedData;
-            }
-
-            // Handle changes in the contenteditable elements
-            selectedItemData.addEventListener('input', function(event) {
-                const target = event.target;
-                if (target.hasAttribute('data-item-id')) {
-                    const itemId = parseInt(target.getAttribute('data-item-id'));
-                    const selectedItem = items.find(item => item.id === itemId);
-
-                    if (selectedItem) {
-                        if (target.tagName === 'H3') {
-                            selectedItem.title = target.textContent;
-                        } else if (target.tagName === 'P') {
-                            selectedItem.description = target.textContent;
-                        }
-
-                        // Update the sidebar if necessary
-                        renderSelectedItemsData();
-                    }
-                }
-            });
-        });
-    </script> --}}
-
     <script>
         document.addEventListener('DOMContentLoaded', function() {
             const itemList = document.getElementById('itemList');
             const selectedItemData = document.getElementById('selectedItemData');
             const selectedItems = new Set();
+            const itemElements = {}; // to keep track of list items
 
             const items = <?= Auth::user()->definecategories ?>;
 
-            // Populate sidebar with item IDs
-            items.forEach(item => {
-                const listItem = document.createElement('li');
-                listItem.className = 'list-group-item';
-                listItem.textContent = `${item.title}`;
-                listItem.addEventListener('click', () => toggleSelectedItem(item));
-                itemList.appendChild(listItem);
-            });
+            function renderSidebar() {
+                // Clear previous content
+                itemList.innerHTML = '';
+
+                // Append data for selected items to the main content area
+                items.forEach(item => {
+                    const listItem = document.createElement('li');
+                    listItem.className = 'list-group-item';
+                    listItem.textContent = `${item.title}`;
+                    listItem.addEventListener('click', () => toggleSelectedItem(item));
+                    itemList.appendChild(listItem);
+
+                    // Keep track of list items
+                    itemElements[item.id] = listItem;
+                });
+            }
 
             function toggleSelectedItem(item) {
                 // Toggle the selected state
@@ -200,12 +82,13 @@
                     const selectedItem = items.find(item => item.id === itemId);
                     if (selectedItem) {
                         selectedItemData.innerHTML += `
-                    <div class="container mt-4 border rounded p-2">
+                    <div class="container">
                         <div class="row ">
                             <div class="col-md-12">
                                 <div class="mb-3">
-                                    <input type="text" class="form-control" id="title_${selectedItem.id}" name="title" value="${selectedItem.title}">
-                                    <textarea class="form-control" id="description_${selectedItem.id}" name="description" rows="4">${selectedItem.description}</textarea>
+                                    <input type="text" class="form-control mb-2" id="title" name="title" value="${selectedItem.title}">
+                                    <textarea class="form-control tinymce" id="description" name="description" rows="6">${selectedItem.description}</textarea>
+                                    <hr />                                                                                    
                                 </div>
                             </div>
                         </div>
@@ -213,7 +96,26 @@
                 `;
                     }
                 });
+
+                // Highlight the selected items in the sidebar
+                highlightSelectedItems();
             }
+
+
+            function highlightSelectedItems() {
+                // Remove 'selected' class from all items
+                Object.values(itemElements).forEach(item => item.classList.remove('selected'));
+
+                // Add 'selected' class to the selected items
+                selectedItems.forEach(itemId => {
+                    if (itemElements[itemId]) {
+                        itemElements[itemId].classList.add('selected');
+                    }
+                });
+            }
+
+            // Initial rendering of the sidebar
+            renderSidebar();
 
             // Button click event handler
             document.getElementById('sendDataButton').addEventListener('click', function() {
@@ -251,4 +153,16 @@
             }
         });
     </script>
+@endsection
+
+@push('script-page-level')
+
 @endpush
+
+
+<style>
+    li.list-group-item.selected {
+        background-color: #007bff;
+        color: white;
+    }
+</style>
