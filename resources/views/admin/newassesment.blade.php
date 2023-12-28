03ac8599b9ef735962f928dc4a7cc7c03ea6751c
Abdul Hannan Danish <mrahdanish@gmail.com>
2023-12-28 15:05:42 +0500

    Add the functionality

--- resources/views/admin/newassesment.blade.php
+++ resources/views/admin/newassesment.blade.php
@@ -8,6 +8,7 @@
         }
     </style>
 @endpush
+
 @section('content')
     <!-- Row start -->
     <div class="row">
@@ -18,20 +19,74 @@
                 </div>
                 <div class="card-body">
                     <div class="row">
-                        <!-- Sidebar -->
-                        <div class="col-3">
-                            <ul id="itemList" class="list-group">
-                                <!-- Item IDs will be dynamically added here -->
+                        @if (Auth::user()->definetopic !== null && count(Auth::user()->definetopic) > 0)
+                            <ul class="nav nav-tabs" id="customTabs" role="tablist">
+                                @foreach (Auth::user()->definetopic as $key => $topic)
+                                    <li class="nav-item" role="presentation">
+                                        <a class="nav-link {{ $key === 0 ? 'active' : '' }}" data-bs-toggle="tab"
+                                            href="#tab-{{ $topic->id }}" role="tab"
+                                            aria-controls="tab-{{ $topic->id }}"
+                                            aria-selected="{{ $key === 0 ? 'true' : 'false' }}">{{ $topic->title }}
+                                            @php
+                                                // Count the categories for the current topic
+                                                $categoryCount = Auth::user()
+                                                    ->definecategories->where('topic_id', $topic->id)
+                                                    ->count();
+                                            @endphp
+                                            @if ($categoryCount > 0)
+                                                <span class="badge bg-info">{{ $categoryCount }}</span>
+                                            @endif
+                                        </a>
+                                    </li>
+                                @endforeach
                             </ul>
-                        </div>
+                            <div class="col-3">
+                                <div class="tab-content" id="customTabContent">
+                                    @foreach (Auth::user()->definetopic as $key => $topic)
+                                        <div class="tab-pane fade {{ $key === 0 ? 'active show' : '' }}"
+                                            id="tab-{{ $topic->id }}" role="tabpanel"
+                                            aria-labelledby="tab-{{ $topic->id }}">
 
-                        <!-- Main Content -->
-                        <div class="col-9">
-                            <div id="selectedItemData" contenteditable="true">
-                                <!-- Selected item's data will be dynamically added here -->
+                                            <ul id="itemList" class="list-group">
+                                                <!-- Item IDs will be dynamically added here -->
+                                                @php
+                                                    // Group categories by their 'group' attribute
+                                                    $groupedCategories = Auth::user()
+                                                        ->definecategories->where('topic_id', $topic->id)
+                                                        ->groupBy('group');
+                                                @endphp
+                                                @forelse ($groupedCategories as $group => $categories)
+                                                    <h5>{{ $group }}</h5>
+                                                    @foreach ($categories as $category)
+                                                        <li class="list-group-item">
+                                                            <span class="category-title"
+                                                                data-category-id="{{ $category->id }}">{{ $category->title }}</span>
+                                                        </li>
+                                                    @endforeach
+                                                @empty
+                                                    <p>No categories available for this topic.</p>
+                                                @endforelse
+                                            </ul>
+
+
+                                            <p>
+                                                @if ($categoryCount > 0)
+                                                    Number of categories for this topic: {{ $categoryCount }}
+                                                @else
+                                                    No categories available for this topic.
+                                                @endif
+                                            </p>
+                                        </div>
+                                    @endforeach
+                                </div>
                             </div>
-                            <!-- Button to Send Selected Data -->
-                        </div>
+                            <div class="col-9 mt-2">
+                                <div id="selectedItemData" contenteditable="true">
+                                    <!-- Selected item's data will be dynamically added here -->
+                                </div>
+                                <!-- Button to Send Selected Data -->
+                            </div>
+                        @endif
                     </div>
                 </div>
                 <div class="card-footer">
@@ -48,38 +103,13 @@
     <script>
         $(document).ready(function() {
             const itemList = $('#itemList');
+            const categoryTitles = $('.category-title');
             const selectedItemData = $('#selectedItemData');
             const selectedItems = new Set();
             const itemElements = {}; // to keep track of list items
 
             const items = <?= Auth::user()->definecategories ?>;
 
-            function renderSidebar() {
-                // Clear previous content
-                itemList.empty();
-
-                // Append data for selected items to the main content area
-                items.forEach(item => {
-                    const listItem = $('<li>').addClass('list-group-item').text(`${item.title}`);
-                    listItem.click(() => toggleSelectedItem(item));
-                    itemList.append(listItem);
-
-                    // Keep track of list items
-                    itemElements[item.id] = listItem[0];
-                });
-            }
-
-            function toggleSelectedItem(item) {
-                // Toggle the selected state
-                if (selectedItems.has(item.id)) {
-                    selectedItems.delete(item.id);
-                } else {
-                    selectedItems.add(item.id);
-                }
-
-                // Render selected items' data in the main content area
-                renderSelectedItemsData();
-            }
             // Function to initialize TinyMCE for a specific element
             function initializeTinyMCE(elementId) {
                 tinymce.init({
@@ -110,18 +140,18 @@
                     if (selectedItem) {
                         const elementId = `description_${selectedItem.id}`;
                         selectedItemData.append(`
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="mb-3">
-                                <input type="text" class="form-control mb-2" id="title_${selectedItem.id}" name="title" value="${selectedItem.title}">
-                                <textarea class="form-control tinymce" id="${elementId}" name="description" rows="6">${selectedItem.description}</textarea>
-                                <hr />
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="mb-3">
+                                    <input type="text" class="form-control mb-2" id="title_${selectedItem.id}" name="title" value="${selectedItem.title}">
+                                    <textarea class="form-control tinymce" id="${elementId}" name="description" rows="6">${selectedItem.description}</textarea>
+                                    <hr />
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            `);
+                `);
 
                         // Initialize TinyMCE for the current element
                         initializeTinyMCE(elementId);
@@ -132,24 +162,43 @@
                 highlightSelectedItems();
             }
 
-
-
             function highlightSelectedItems() {
                 // Remove 'selected' class from all items
-                Object.values(itemElements).forEach(item => $(item).removeClass('selected'));
+                itemList.find('li').removeClass('selected');
 
                 // Add 'selected' class to the selected items
                 selectedItems.forEach(itemId => {
-                    if (itemElements[itemId]) {
-                        $(itemElements[itemId]).addClass('selected');
+                    const listItem = itemElements[itemId];
+                    if (listItem) {
+                        $(listItem).addClass('selected');
                     }
                 });
             }
 
+            // Attach click event to category titles
+            categoryTitles.click(function() {
+                const categoryId = $(this).data('category-id');
 
-            // Initial rendering of the sidebar
-            renderSidebar();
+                // Toggle the 'selected' class for the clicked category
+                $(this).toggleClass('selected');
 
+                // Update the selected items list based on the 'selected' class
+                if ($(this).hasClass('selected')) {
+                    selectedItems.add(categoryId);
+                } else {
+                    selectedItems.delete(categoryId);
+                }
+
+                // Render selected items' data in the main content area
+                renderSelectedItemsData();
+            });
+
+
+
+
+
+
+            categoryTitles.css('cursor', 'pointer');
             // Button click event handler
             $('#sendDataButton').click(function() {
                 // Get data of selected items
@@ -160,29 +209,6 @@
                     selectedData));
             });
 
-            //     function getSelectedData() {
-            //         const selectedData = [];
-
-            //         // Collect data for selected items
-            //         selectedItems.forEach(itemId => {
-            //             const selectedItem = items.find(item => item.id === itemId);
-            //             if (selectedItem) {
-            //                 // Get the modified values from the input fields
-            //                 const modifiedTitle = $(`#title_${selectedItem.id}`).val();
-            //                 const modifiedDescription = $(`#description_${selectedItem.id}`).val();
-
-            //                 // Add modified data to the selectedData array
-            //                 selectedData.push({
-            //                     id: selectedItem.id,
-            //                     title: modifiedTitle,
-            //                     description: modifiedDescription,
-            //                     type: selectedItem.type,
-            //                 });
-            //             }
-            //         });
-
-            //         return selectedData;
-            //     }
             function getSelectedData() {
                 const selectedData = [];
 
